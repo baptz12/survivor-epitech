0606af4273cd8c8ec8c50dc477377342bfcf656c
baptz12 <baptiste.zetler@epitech.eu>
2026-03-25 20:16:52 +0100

    [FIX]: fix svelte adapter and remove vercel.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+	import adapter from '@sveltejs/adapter-auto';
+	import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	/** @type {import('@sveltejs/kit').Config} */
+	const config = {
+		// Consult https://svelte.dev/docs/kit/integrations
+		// for more information about preprocessors
+		preprocess: vitePreprocess(),
 
-	kit: {
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/survivor-epitech-web'
-		},
-		adapter: adapter({
-			// Fallback est indispensable pour que GitHub Pages gère le routage interne de ton app
-			fallback: '404.html'
-		})
-	}
-};
+		kit: {
+			adapter: adapter()
+		}
+	};
 
-export default config;
+	export default config;
